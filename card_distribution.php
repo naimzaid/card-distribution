35b31de11e44178565ac8c3884fe9c93761d1dce
Naim Zaid <m.naim.zaid@gmail.com>
2023-02-13 22:25:10 +0800

    include message for out of cards

--- card_distribution.php
+++ card_distribution.php
@@ -8,9 +8,18 @@ $people = $card->distributeCards();
 
 // Printing the cards distributed to each person
 for ($i = 0; $i < $card->getNumOfPeople(); $i++) {
+    if ($i < 52){
+        echo "Person " . ($i + 1) . ": ";
+        echo implode(",", $people[$i]) . "\n";
+        echo "<br>";
+        continue;
+    }
+
+    // Run out of cards
     echo "Person " . ($i + 1) . ": ";
-    echo implode(",", $people[$i]) . "\n";
+    echo "Out of cards";
     echo "<br>";
+    
 }
 
 ?>
